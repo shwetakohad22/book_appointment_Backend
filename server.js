ec7c3697e7a7fc96ececfa0dffc08bb159ac38d1
shwetakohad22 <shwetakohad22@gmail.com>
2024-02-29 14:38:23 +0530

    remove production statement

--- server.js
+++ server.js
@@ -20,13 +20,5 @@ app.use("/api/doctor", doctorRoute);
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
-
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
